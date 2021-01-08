0e185fd64030c7f378b6b81f7d2a8d8cf218cbc5
william koller <wkoller25@gmail.com>
2021-01-07 23:16:58 -0300

    feat: set TypeOrmModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { configService } from './config/config.service'
+import { UserModule } from './user/user.module'
 
 @Module({
-  imports: [TypeOrmModule.forRoot(configService.getTypeOrmConfig()[0])],
+  imports: [TypeOrmModule.forRoot(configService.getTypeOrmConfig()[0]), UserModule],
   controllers: [],
   providers: [],
 })
