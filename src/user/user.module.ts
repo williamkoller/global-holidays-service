a2203ea54e74e15c7d0c6d13f363738577d93625
william koller <wkoller25@gmail.com>
2021-01-08 01:40:07 -0300

    feat: update UserModule

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { User } from 'src/entities/user.entity'
-import { UserService } from './service/user.service'
-import { UserController } from './controller/user.controller'
+import { UserService } from './services/user.service'
+import { UserController } from './controllers/user.controller'
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
