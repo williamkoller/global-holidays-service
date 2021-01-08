b85ad06fafe382ebf572206907992928c6f30867
william koller <wkoller25@gmail.com>
2021-01-08 01:38:27 -0300

    feta: add TypeOrmModule on AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { configService } from './config/config.service'
 import { UserModule } from './user/user.module'
+import { AuthModule } from './auth/auth.module'
 
 @Module({
-  imports: [TypeOrmModule.forRoot(configService.getTypeOrmConfig()[0]), UserModule],
+  imports: [TypeOrmModule.forRoot(configService.getTypeOrmConfig()[0]), UserModule, AuthModule],
   controllers: [],
   providers: [],
 })
