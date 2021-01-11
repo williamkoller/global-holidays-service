2acc13e6736570acaab398e4e396627a9df5aad9
william koller <wkoller25@gmail.com>
2021-01-10 21:03:06 -0300

    feat: add authenticateUser

--- src/shared/aws/aws.service.ts
+++ src/shared/aws/aws.service.ts
@@ -1,9 +1,9 @@
 import { Inject, Injectable } from '@nestjs/common'
-import { CognitoUserPool } from 'amazon-cognito-identity-js'
 import { CognitoIdentityServiceProvider } from 'aws-sdk'
-import { UserType } from 'aws-sdk/clients/cognitoidentityserviceprovider'
+import { AuthenticationDetails, CognitoUser, CognitoUserPool } from 'amazon-cognito-identity-js'
+
 import { AuthConfig } from 'src/auth/config/auth.config'
-import { CognitoUser } from 'src/auth/interface/cognito-user.interface'
+import { CreateCognitoUserDto } from '../dtos/create-cognito-user.dto'
 
 @Injectable()
 export class AwsService {
@@ -21,8 +21,58 @@ export class AwsService {
     })
   }
 
-  public async registerUser(registerRequest: { name: string; email: string; password: string }) {
+  public async registerUser(registerRequest: { email: string; password: string }): Promise<{ UserSub: string }> {
     const { email, password } = registerRequest
+    return this.cognito
+      .signUp({
+        ClientId: this.authConfig.clientId,
+        Username: email,
+        Password: password,
+        UserAttributes: [{ Name: 'email', Value: email }],
+      })
+      .promise()
+  }
+
+  authenticateUser(user: { username: string; password: string }) {
+    const { username, password } = user
+
+    const authenticationDetails = new AuthenticationDetails({
+      Username: username,
+      Password: password,
+    })
+    const userData = {
+      Username: username,
+      Pool: this.userPool,
+    }
+
+    const newUser = new CognitoUser(userData)
+
+    return new Promise((resolve, reject) => {
+      return newUser.authenticateUser(authenticationDetails, {
+        onSuccess: (result) => {
+          resolve(result)
+        },
+        onFailure: (err) => {
+          reject(err)
+        },
+      })
+    })
+  }
+
+  public async checkIfUserExists({ email }: { email: string }): Promise<any> {
+    const response = await this.cognito
+      .listUsers({
+        UserPoolId: this.authConfig.userPoolId,
+        Limit: 1,
+        Filter: `email="${email}"`,
+      })
+      .promise()
+
+    return response
+  }
+
+  public async createCognitoUser(createCognitoUserDto: CreateCognitoUserDto): Promise<any> {
+    const { email, password } = createCognitoUserDto
     const response = await this.cognito
       .adminCreateUser({
         UserPoolId: this.authConfig.userPoolId,
@@ -34,82 +84,12 @@ export class AwsService {
             Value: email,
           },
           {
-            // Don't verify email addresses
             Name: 'email_verified',
             Value: 'true',
           },
         ],
       })
       .promise()
-
     return response
-  }
-
-  public async checkIfUserExists({ email }: { email: string }): Promise<CognitoUser | undefined> {
-    const response = await this.cognito
-      .listUsers({
-        UserPoolId: this.authConfig.userPoolId,
-        Limit: 1,
-        Filter: `email="${email}"`,
-      })
-      .promise()
-
-    return this.formatUser(response.Users[0])
-  }
-
-  public async createCognitoUser(email: string): Promise<CognitoUser | undefined> {
-    const response = await this.cognito
-      .adminCreateUser({
-        UserPoolId: this.authConfig.userPoolId,
-        Username: email,
-        TemporaryPassword: this.passwordGenerator(8),
-        UserAttributes: [
-          {
-            Name: 'email',
-            Value: email,
-          },
-          {
-            // Don't verify email addresses
-            Name: 'email_verified',
-            Value: 'true',
-          },
-        ],
-      })
-      .promise()
-    return this.formatUser(response.User)
-  }
-
-  private formatUser(user?: UserType): CognitoUser | undefined {
-    return user
-      ? {
-          sub: user.Username,
-          email: user.Attributes.find((attr) => attr.Name === 'email').Value,
-        }
-      : undefined
-  }
-
-  private passwordGenerator(len: number): string {
-    const length = len ? len : 10
-    const characters = 'abcdefghijklmnopqrstuvwxyz'
-    const numeric = '0123456789'
-    const punctuation = '!@#$%^&*()_+~`|}{[]:;?><,./-='
-    let password = ''
-    let character = ''
-    while (password.length < length) {
-      const entity1 = Math.ceil(characters.length * Math.random() * Math.random())
-      const entity2 = Math.ceil(numeric.length * Math.random() * Math.random())
-      const entity3 = Math.ceil(punctuation.length * Math.random() * Math.random())
-      let hold = characters.charAt(entity1)
-      hold = password.length % 2 === 0 ? hold.toUpperCase() : hold
-      character += hold
-      character += numeric.charAt(entity2)
-      character += punctuation.charAt(entity3)
-      password = character
-    }
-    password = password
-      .split('')
-      .sort(() => 0.5 - Math.random())
-      .join('')
-    return password.substr(0, len)
   }
 }
