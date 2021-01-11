b1cd36657e198c41fcea47306fb7e8a37130a97d
william koller <wkoller25@gmail.com>
2021-01-10 21:00:33 -0300

    feta: add PassportModule and JwtModule

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,11 +4,24 @@ import { AuthConfig } from './config/auth.config'
 import { AuthService } from './services/auth.service'
 import { AuthController } from './controllers/auth.controller'
 import { UserModule } from 'src/user/user.module'
+import { JwtStrategy } from './strategies/jwt.strategy'
+import { PassportModule } from '@nestjs/passport'
+import { JwtModule } from '@nestjs/jwt'
+import { jwtConstants } from '../auth/constants/constants'
 
 @Module({
-  imports: [forwardRef(() => UserModule)],
-  providers: [AuthConfig, AuthService, AwsService],
-  exports: [AuthService, AuthConfig],
+  imports: [
+    forwardRef(() => UserModule),
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.register({
+      secret: jwtConstants.secret,
+      signOptions: {
+        expiresIn: '1d',
+      },
+    }),
+  ],
+  providers: [AuthConfig, AuthService, AwsService, JwtStrategy],
+  exports: [AuthService, AuthConfig, AwsService],
   controllers: [AuthController],
 })
 export class AuthModule {}
