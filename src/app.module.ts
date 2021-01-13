e840dbcf167685f64685b76dc109e9e7d5125762
william koller <wkoller25@gmail.com>
2021-01-12 23:30:46 -0300

    feat: add ContinentModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { configService } from './config/config.service'
 import { AuthModule } from './auth/auth.module'
+import { ContinentModule } from './continent/continent.module'
 
 @Module({
-  imports: [TypeOrmModule.forRoot(configService.getTypeOrmConfig()[0]), AuthModule],
+  imports: [TypeOrmModule.forRoot(configService.getTypeOrmConfig()[0]), AuthModule, ContinentModule],
   controllers: [],
   providers: [],
 })
