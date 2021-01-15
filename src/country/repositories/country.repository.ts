66c46d12eb517e336e9f2eb5d9d7b48dd1b18eb0
william koller <wkoller25@gmail.com>
2021-01-15 00:32:08 -0300

    feat: should be throw find returns empty

--- src/country/repositories/country.repository.ts
+++ src/country/repositories/country.repository.ts
@@ -1,9 +1,14 @@
 import { Country } from '../../entities/country.entity';
 import { EntityRepository, Repository } from 'typeorm';
+import { BadRequestException } from '@nestjs/common';
 
 @EntityRepository(Country)
 export class CountryRepository extends Repository<Country> {
   async getAllCountry(): Promise<Country[]> {
-    return await this.find();
+    const result = await this.find();
+    if (!result) {
+      throw new BadRequestException('The Country not found.');
+    }
+    return result;
   }
 }
