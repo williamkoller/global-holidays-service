f3000a5c3db790fc50495536a14f84ed2b8908b0
william koller <wkoller25@gmail.com>
2021-01-14 21:55:41 -0300

    chore: formated eslint

--- src/country/country.module.ts
+++ src/country/country.module.ts
@@ -1,10 +1,10 @@
-import { forwardRef, Module } from '@nestjs/common'
-import { CountryService } from './services/country.service'
-import { CountryController } from './controllers/country.controller'
-import { TypeOrmModule } from '@nestjs/typeorm'
-import { Country } from 'src/entities/country.entity'
-import { ContinentModule } from 'src/continent/continent.module'
-import { CountryRepository } from './repositories/country.repository'
+import { forwardRef, Module } from '@nestjs/common';
+import { CountryService } from './services/country.service';
+import { CountryController } from './controllers/country.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Country } from 'src/entities/country.entity';
+import { ContinentModule } from 'src/continent/continent.module';
+import { CountryRepository } from './repositories/country.repository';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Country]), forwardRef(() => ContinentModule)],
