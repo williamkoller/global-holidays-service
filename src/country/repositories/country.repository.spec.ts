92a314a70f04455a5e154cc7175c1852011a601b
william koller <wkoller25@gmail.com>
2021-01-14 21:56:16 -0300

    test: should be find correct params

--- src/country/repositories/country.repository.spec.ts
+++ src/country/repositories/country.repository.spec.ts
@@ -1,18 +1,27 @@
-import { Test, TestingModule } from '@nestjs/testing'
-import { CountryRepository } from './country.repository'
+import { Test, TestingModule } from '@nestjs/testing';
+import { CountryRepository } from './country.repository';
 
 describe('CountryRepository', () => {
-  let repository: CountryRepository
+  let repository;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      controllers: [CountryRepository],
-    }).compile()
+      providers: [CountryRepository],
+    }).compile();
 
-    repository = module.get<CountryRepository>(CountryRepository)
-  })
+    repository = module.get<CountryRepository>(CountryRepository);
+    repository.find = jest.fn();
+  });
 
   it('should be defined', () => {
-    expect(repository).toBeDefined()
-  })
-})
+    expect(repository).toBeDefined();
+  });
+
+  describe('getAllCountry', () => {
+    it('should be find with correct params', async () => {
+      repository.find = jest.fn().mockReturnValue({});
+      await repository.getAllCountry();
+      expect(repository.find).toBeCalledWith();
+    });
+  });
+});
