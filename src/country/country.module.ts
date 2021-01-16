e56a2b73bfaf4db7e619341dde191d8a5bb539b4
william koller <wkoller25@gmail.com>
2021-01-16 15:19:17 -0300

    feat: set providers CreateCountryRepository

--- src/country/country.module.ts
+++ src/country/country.module.ts
@@ -5,10 +5,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Country } from 'src/entities/country.entity';
 import { ContinentModule } from 'src/continent/continent.module';
 import { FindCountryRepository } from './repositories/find-country/find-country.repository';
+import { CreateCountryRepository } from './repositories/create-country/create-country.repository';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Country]), forwardRef(() => ContinentModule)],
-  providers: [CountryService, FindCountryRepository],
+  providers: [CountryService, FindCountryRepository, CreateCountryRepository],
   controllers: [CountryController],
 })
 export class CountryModule {}
