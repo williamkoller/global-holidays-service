ad3df97ded97f107e77890ad0429a039970074fa
william koller <wkoller25@gmail.com>
2021-01-17 03:13:13 -0300

    feat: should be throw when save throw

--- src/country/repositories/add-country/add-country.repository.ts
+++ src/country/repositories/add-country/add-country.repository.ts
@@ -1,10 +1,15 @@
 import { Country } from '../../../entities/country.entity';
 import { EntityRepository, Repository } from 'typeorm';
 import { CreateCountryDto } from '../../dtos/create-country.dto';
+import { InternalServerErrorException } from '@nestjs/common';
 
 @EntityRepository(Country)
 export class AddCountryRepository extends Repository<Country> {
-  async addCountry(data: CreateCountryDto): Promise<Country> {
-    return await this.save(data);
+  async addCountry(createCountryDto: CreateCountryDto): Promise<Country> {
+    try {
+      return await this.save(createCountryDto);
+    } catch (e) {
+      throw new InternalServerErrorException(e);
+    }
   }
 }
