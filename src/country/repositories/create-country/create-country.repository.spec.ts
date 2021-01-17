0cf59577ff6d50c78cb9114d1807e86576fb7e70
william koller <wkoller25@gmail.com>
2021-01-17 01:25:47 -0300

    test: should be called save with correct params

--- src/country/repositories/create-country/create-country.repository.spec.ts
+++ src/country/repositories/create-country/create-country.repository.spec.ts
@@ -3,6 +3,7 @@ import { CreateCountryRepository } from './create-country.repository';
 
 describe('CreateCountryRepository', () => {
   let repository;
+  let mockData;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -10,8 +11,26 @@ describe('CreateCountryRepository', () => {
     }).compile();
 
     repository = module.get<CreateCountryRepository>(CreateCountryRepository);
+    repository.save = jest.fn();
+    mockData = [
+      {
+        name: 'Brazil',
+        capitalContry: 'Brasilia',
+        territorialExtension: 8510295914,
+        localization: 'South America',
+        language: 'Portuguese',
+        currency: 'Real',
+        continent: 'Latin America',
+      },
+    ];
   });
   it('should be defined', () => {
     expect(repository).toBeDefined();
+  });
+
+  it('should be called save with correct params', async () => {
+    repository.save = jest.fn().mockReturnValue(mockData);
+    await repository.createCountry(mockData);
+    expect(repository.save).toBeCalledWith(mockData);
   });
 });
