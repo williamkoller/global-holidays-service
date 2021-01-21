81f30c784f3b35afd56852cb2546dcd1a04155cd
william koller <wkoller25@gmail.com>
2021-01-21 01:19:31 -0300

    feat: should be throw if repository throw

---
+++ src/country/services/add-country/add-country.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@nestjs/common';
+import { CreateCountryDto } from '../../../country/dtos/create-country.dto';
+import { Country } from '../../../entities/country.entity';
+import { AddCountryRepository } from '../../../country/repositories/export';
+
+@Injectable()
+export class AddCountryService {
+  constructor(private readonly addCountryRepository: AddCountryRepository) {}
+
+  async addCountry(data: CreateCountryDto): Promise<Country> {
+    return await this.addCountryRepository.addCountry(data);
+  }
+}
