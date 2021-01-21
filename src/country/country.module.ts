dcddb4d369ef530804425fc632f890a3e45de77d
william koller <wkoller25@gmail.com>
2021-01-21 01:21:30 -0300

    feat: update CountryModule

--- src/country/country.module.ts
+++ src/country/country.module.ts
@@ -1,15 +1,14 @@
 import { forwardRef, Module } from '@nestjs/common';
-import { CountryService } from './services/country.service';
 import { CountryController } from './controllers/country.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Country } from 'src/entities/country.entity';
 import { ContinentModule } from 'src/continent/continent.module';
-import { LoadAllCountryRepository } from './repositories/load-all-country/load-all-country.repository';
-import { AddCountryRepository } from './repositories/add-country/add-country.repository';
+import { LoadAllCountryRepository, AddCountryRepository } from './repositories/export';
+import { LoadAllCountryService, AddCountryService } from './services/export';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Country]), forwardRef(() => ContinentModule)],
-  providers: [CountryService, LoadAllCountryRepository, AddCountryRepository],
+  providers: [AddCountryService, LoadAllCountryService, LoadAllCountryRepository, AddCountryRepository],
   controllers: [CountryController],
 })
 export class CountryModule {}
