e5aae39019a6ac8f9e511e8323aca6d73eb0d6e8
william koller <wkoller25@gmail.com>
2021-01-21 01:17:40 -0300

    test: should be throw if repository throw

---
+++ src/country/services/load-all-country/load-all-country.service.spec.ts
@@ -0,0 +1,38 @@
+import { InternalServerErrorException } from '@nestjs/common';
+import { Test, TestingModule } from '@nestjs/testing';
+import { LoadAllCountryRepository } from '../../../country/repositories/export';
+import { LoadAllCountryService } from '../export';
+
+describe('LoadAllCountryService', () => {
+  let service: LoadAllCountryService;
+  let repository: LoadAllCountryRepository;
+
+  beforeEach(async () => {
+    const loadAllCountryServiceMock = {
+      loadAllCountry: jest.fn(),
+    };
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        LoadAllCountryService,
+        {
+          provide: LoadAllCountryRepository,
+          useFactory: () => loadAllCountryServiceMock,
+        },
+      ],
+    }).compile();
+
+    service = module.get<LoadAllCountryService>(LoadAllCountryService);
+    repository = module.get<LoadAllCountryRepository>(LoadAllCountryRepository);
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+
+  describe('loadAllCountry', () => {
+    it('should be throw if repository throw', async () => {
+      (repository.loadAllCountry as jest.Mock).mockRejectedValue(new InternalServerErrorException());
+      await expect(service.loadAllCountry()).rejects.toThrow(new InternalServerErrorException());
+    });
+  });
+});
